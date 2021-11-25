d047e614f9d8f266e905ac694ac7504fde58cd2c
OjeshManandhar <ozes.manandhar@gmail.com>
2021-11-25 22:51:21 +0545

    Reset DB

--- prisma/migrations/20211125170145_init/migration.sql
+++ prisma/migrations/20211125170145_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "deletedAt" DATETIME
