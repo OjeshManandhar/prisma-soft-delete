0d3986f6634d0616c8b12ff54ae7f5869315dd2f
OjeshManandhar <ozes.manandhar@gmail.com>
2021-12-03 19:42:04 +0545

    Make is field nullable

---
+++ prisma/migrations/20211203135640_make_is_field_nullable/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - Made the column `is` on table `Comment` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "opinion" TEXT NOT NULL,
+    "isReply" BOOLEAN NOT NULL,
+    "is" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "postId" TEXT,
+    "parentCommentId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME,
+    CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Comment_parentCommentId_fkey" FOREIGN KEY ("parentCommentId") REFERENCES "Comment" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Comment" ("authorId", "createdAt", "deletedAt", "id", "is", "isReply", "opinion", "parentCommentId", "postId", "updatedAt") SELECT "authorId", "createdAt", "deletedAt", "id", "is", "isReply", "opinion", "parentCommentId", "postId", "updatedAt" FROM "Comment";
+DROP TABLE "Comment";
+ALTER TABLE "new_Comment" RENAME TO "Comment";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
